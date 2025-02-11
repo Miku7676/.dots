de31271c99bb0572b71b024f6c057ff34ea46761
Miku7676 <biswabmiku@gmail.com>
2025-02-11 16:23:09 +0530

    added window , tab remaps

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -12,3 +12,55 @@ vim.cmd("set shiftwidth=2")
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- go to netrw view
 vim.keymap.set("n", "<Esc>", '<cmd>nohlsearch<CR>')
 vim.keymap.set("n", "<leader><leader>", function() vim.cmd("so") end)
+
+--opening a new pane
+vim.keymap.set("n", "<leader>|", function()
+  require('telescope.builtin').find_files({
+    attach_mappings = function(prompt_bufnr, map)
+      local actions = require('telescope.actions')
+      local action_state = require('telescope.actions.state')
+
+      map('i', '<CR>', function()
+        local selection = action_state.get_selected_entry()
+        if selection and selection.path then
+          actions.close(prompt_bufnr)  -- Close first to avoid picker issues
+          vim.cmd("vert new " .. vim.fn.fnameescape(selection.path))
+        end
+      end)
+
+      return true
+    end
+  })
+end, { noremap = true, silent = true })
+
+-- horizontal
+vim.keymap.set("n", "<leader>-", function()
+  require('telescope.builtin').find_files({
+    attach_mappings = function(prompt_bufnr, map)
+      local actions = require('telescope.actions')
+      local action_state = require('telescope.actions.state')
+
+      map('i', '<CR>', function()
+        local selection = action_state.get_selected_entry()
+        if selection and selection.path then
+          actions.close(prompt_bufnr)  -- Close first to avoid picker issues
+          vim.cmd("new " .. vim.fn.fnameescape(selection.path))
+        end
+      end)
+
+      return true
+    end
+  })
+end, { noremap = true, silent = true })
+
+-- tab remaps
+vim.keymap.set("n", "<leader>1", "1gt", {noremap = true, silent = true})
+vim.keymap.set("n", "<leader>2", "2gt", {noremap = true, silent = true})
+vim.keymap.set("n", "<leader>3", "3gt", {noremap = true, silent = true})
+vim.keymap.set("n", "<leader>4", "4gt", {noremap = true, silent = true})
+-- vim.keymap.set("n", "<leader>5", "5gt", {noremap = true, silent = true})
+-- vim.keymap.set("n", "<leader>6", "6gt", {noremap = true, silent = true})
+-- vim.keymap.set("n", "<leader>7", "7gt", {noremap = true, silent = true})
+-- vim.keymap.set("n", "<leader>8", "8gt", {noremap = true, silent = true})
+-- vim.keymap.set("n", "<leader>9", "9gt", {noremap = true, silent = true})
+vim.keymap.set("n", "<leader>0", ":tablast<cr>", {noremap = true, silent = true})
