8f1920812aecd742c7b4d51665a859690972f6f5
Miku7676 <biswabmiku@gmail.com>
2025-03-07 23:22:49 +0530

    some nvim remaps

---
+++ bash/.bash_aliases
@@ -0,0 +1,4 @@
+alias vim="nvim"
+alias go='go.exe'
+alias cls='clear'
+alias ff='find-fil-dir'
--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -2,13 +2,16 @@ vim.g.mapleader = " "
 vim.api.nvim_set_keymap('i', '<Esc>', '<Esc>', { noremap = true, silent = true })
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.list = true
-vim.opt.listchars = {tab = '..', trail = '·', nbsp = '␣' }
+vim.opt.showmode = false
+vim.opt.breakindent = true
+-- vim.opt.list = true
+-- vim.opt.listchars = {trail = '·', nbsp = '␣'} -- tab = '..' }
 vim.opt.scrolloff = 10
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- go to netrw view
 vim.keymap.set("n", "<Esc>", '<cmd>nohlsearch<CR>')
 vim.keymap.set("n", "<leader><leader>", function() vim.cmd("so") end)
